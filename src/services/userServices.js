23bd3e1a5fb099c60a6eea02c00a1b9bd8d22fe8
josef gisis <jsquared.gisis@gmail.com>
2024-01-29 12:36:14 -0500

    removed services

--- src/services/userServices.js
+++
@@ -1,8 +0,0 @@
-export default {
-    async fetchUsers() {
-        return fetch('http://localhost:3000/api/users')
-            .then(res => {return res.json()})
-            .then(data => {return data})
-            .catch(err => console.log(err))
-    }
-}
