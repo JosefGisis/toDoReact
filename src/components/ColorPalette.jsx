d5da377e59640e6d47cef2aa59f5b983c2c50224
josef gisis <jsquared.gisis@gmail.com>
2024-02-27 11:14:26 -0500

    improved colorPalette and spinner

--- src/components/ColorPalette.jsx
+++ src/components/ColorPalette.jsx
@@ -1,63 +1,25 @@
 export default function ColorPalette() {
+	const colors = [
+		'bg-primary',
+		'bg-secondary',
+		'bg-accent',
+		'bg-neutral',
+		'bg-base-100',
+		'bg-base-200',
+		'bg-base-300',
+		'bg-info',
+		'bg-success',
+		'bg-warning',
+		'bg-error',
+	]
+
 	return (
 		<div className="m-10">
-			<div className="bg-primary">
-				<p>bg-primary</p>
-			</div>
-			<div className="bg-primary-content">
-				<p>bg-primary-content</p>
-			</div>
-			<div className="bg-secondary">
-				<p>bg-secondary</p>
-			</div>
-			<div className="bg-secondary-content">
-				<p>bg-secondary-content</p>
-			</div>
-			<div className="bg-accent">
-				<p>bg-accent</p>
-			</div>
-			<div className="bg-accent-content">
-				<p>bg-accent-content</p>
-			</div>
-			<div className="neutral">
-				<p>neutral</p>
-			</div>
-			<div className="bg-base-100">
-				<p>base-100</p>
-			</div>
-			<div className="bg-base-200">
-				<p>base-200</p>
-			</div>
-			<div className="bg-base-300">
-				<p>base-300</p>
-			</div>
-			<div className="bg-base-content">
-				<p>base-content</p>
-			</div>
-			<div className="bg-info">
-				<p>info</p>
-			</div>
-			<div className="bg-info-content">
-				<p>info-content</p>
-			</div>
-			<div className="bg-success">
-				<p>success</p>
-			</div>
-			<div className="bg-success-content">
-				<p>success-content</p>
-			</div>
-			<div className="bg-warning">
-				<p>warning</p>
-			</div>
-			<div className="bg-warning-content">
-				<p>warning-content</p>
-			</div>
-			<div className="bg-error">
-				<p>error</p>
-			</div>
-			<div className="bg-error-content">
-				<p>error-content</p>
-			</div>
+			{colors.map((color, index) => (
+				<div key={index} className={color}>
+					<p>{color}</p>
+				</div>
+			))}
 		</div>
 	)
 }
--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,7 +1,7 @@
 export default function Spinner() {
 	return (
   <div className='absolute w-[100%] left-0 flex justify-center'>
-    <div className="animate-spin h-12 w-12 border-t-4 border-b-4 border-sky-400 rounded-full"></div>
+    <div className="animate-spin h-12 w-12 border-t-4 border-b-4 border-primary rounded-full"></div>
   </div>
   )
 }
