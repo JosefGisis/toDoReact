f644304f24db45ecc789efa287f740ad0118ba78
josef gisis <jsquared.gisis@gmail.com>
2024-02-28 09:39:28 -0500

    changed base_url and avatar color

--- src/features/Navigation/components/Avatar.jsx
+++ src/features/Navigation/components/Avatar.jsx
@@ -8,10 +8,10 @@ export default function Avatar() {
 
 	function getAvatarColor() {
 		const currentHour = new Date().getHours()
-		if (currentHour < 24 && currentHour >= 18) return 'bg-secondary'
-		if (currentHour < 18 && currentHour >= 12) return 'bg-primary'
-		if (currentHour < 12 && currentHour >= 6) return 'bg-accent'
-		return 'bg-success'
+		if (currentHour < 24 && currentHour >= 18) return 'bg-secondary text-secondary-content'
+		if (currentHour < 18 && currentHour >= 12) return 'bg-primary text-primary-content'
+		if (currentHour < 12 && currentHour >= 6) return 'bg-accent text-accent-content'
+		return 'bg-success text-success-content'
 	}
 
 	const avatarColor = useMemo(() => getAvatarColor(), [])
